4e9ee88ff606153f9dd49363f2e0b34b728afb0b
sachin <sachin.singh@manektech.com>
2023-04-10 17:36:33 +0530

    init

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express')
+const cors = require('cors')
 const products = require('./data/products')
 
 const app = express()
+
+//handling cors error
+app.use(cors);
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('API is running...')
