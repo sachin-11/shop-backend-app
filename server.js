7ab422f4277d0c448315bd16dcaa1778bf57f592
sachin <sachin.singh@manektech.com>
2023-04-11 14:34:27 +0530

    init

--- server.js
+++ server.js
@@ -1,37 +1,27 @@
-const express = require('express')
-const cors = require('cors')
-const dotenv = require('dotenv')
-const colors = require('colors')
-const products = require('./data/products')
+const express = require('express');
 
-dotenv.config();
+const app = express();
+const port = 5000; // Port to listen on
 
-const app = express()
-
-//handling cors error
-app.use(cors);
-app.use(express.json())
+// Middleware to parse JSON request bodies
+app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('API is running..')
-})
-
-app.get('/products', (req, res) => {
-  res.json(products)
-})
-
-app.get('/products/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id)
-  res.json(product)
-})
-
-const PORT = process.env.PORT || 5000;
-
-const server = app.listen(PORT, () => {
-    console.log(
-      `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
-    );
-  });
+    res.send('API is running...')
+  })
   
+  app.get('/api/products', (req, res) => {
+    res.json(products)
+  })
+  
+  app.get('/api/products/:id', (req, res) => {
+    const product = products.find((p) => p._id === req.params.id)
+    res.json(product)
+  })
+
+// Start the server
+const server = app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
 
 module.exports = server;
