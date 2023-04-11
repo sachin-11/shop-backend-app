26f0fd1a6778ea05d8f1a961994959c045e92e33
sachin <sachin.singh@manektech.com>
2023-04-11 13:57:58 +0530

    init

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const cors = require('cors')
+const dotenv = require('dotenv')
+const colors = require('colors')
 const products = require('./data/products')
+
+dotenv.config();
 
 const app = express()
 
@@ -21,6 +25,13 @@ app.get('/products/:id', (req, res) => {
   res.json(product)
 })
 
-app.listen(5000, console.log('Server running on port 5000'))
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(PORT, () => {
+    console.log(
+      `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+    );
+  });
+  
 
 module.exports = server;
